0fb71707133dfde4afbd5c7dd0fac4a5d87b6156
Deligent2 <148380705+Deligent2@users.noreply.github.com>
2023-11-16 17:17:36 +0300

    finished

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-const email = document.querySelector('.email')
-const L_name = document.querySelector('.L_name')
-const F_name = document.querySelector('.F_name')
-const U_name = document.querySelector('.U_name')
-const password = document.querySelector('.password')
-const password_1 = document.querySelector('.password_1')
+function validateForm(event) {
+  event.preventDefault();
 
+  const U_name = document.querySelector('.U_name');
+  const F_name = document.querySelector('.f_name');
+  const L_name = document.querySelector('.l_name');
+  const email = document.querySelector('.email');
+  const password = document.querySelector('.Password');
+  const password_1 = document.querySelector('.password_2');
 
-
-
-
-
-
-
-
-function myFunction() {
-    let x = document.getElementById("myInput");
-    let c = document.getElementById("myInput_1");
-    if (x.type === "password" || c.type === "password") {
-      x.type = "text", c.type = "text"
-    } else {
-      x.type = "password", c.type = "password";
-    }
+  if (U_name.value.length < 3 || F_name.value.length < 3 || L_name.value.length < 3) {
+    alert('Минимальная длина поля User Name, First Name и Last Name - 3 символа.');
+    return;
   }
 
+  if (password.value !== password_1.value || password.value.length > 8 || password_1.value.length > 8) {
+    alert('Пароли не совпадают или превышают максимальную длину в 8 символов.');
+    return;
+  }
+
+  let result = 'User Name: ' + U_name.value + '\nFirst Name: ' + F_name.value + '\nLast Name: ' + L_name.value +
+    '\nEmail: ' + email.value + '\nPassword: ' + password.value;
+  alert(result);
+}
+
+function myFunction() {
+  let x = document.getElementById("myInput");
+  let c = document.getElementById("myInput_1");
+  if (x.type === "password" || c.type === "password") {
+    x.type = "text", c.type = "text";
+  } else {
+    x.type = "password", c.type = "password";
+  }
+}
